d8b6fd022c398aca74da1dab2f1b3c70c831153e
Álvaro Pena Ruiz <alvaro.penaruiz@epfl.ch>
2022-12-07 10:34:13 +0100

    day 6 first puzzle

--- 06/src/main.ts
+++ 06/src/main.ts
@@ -1,1 +1,40 @@
 import getInput from "./input";
+
+const cryptedComm = getInput()
+
+const lookForFirstMark = (message: string) => {
+
+	const processedChars: string[] = []
+	let position: number[] = []
+
+	Array.from(message).forEach((char, index) => {
+
+		if (processedChars.length < 4) {
+
+			processedChars.push(char)
+
+		} else {
+
+			if ([... new Set(processedChars)].length < 4) {
+
+				processedChars.shift()
+				processedChars.push(char)
+
+			} else {
+				
+				position.push(index)
+				processedChars.length = 0
+				
+			}
+		}
+
+	})
+
+	return position
+}
+
+
+console.log(cryptedComm)
+
+//Puzzle 1
+console.log(lookForFirstMark(cryptedComm))
